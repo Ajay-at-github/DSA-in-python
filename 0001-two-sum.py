d0fb69c6610c2e516016a31d54bf0b0e05e58a3b
Ajay-at-github <146206087+Ajay-at-github@users.noreply.github.com>
2024-07-19 13:42:11 +0530

    Delete 0001-two-sum.py

--- 0001-two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> list[int]:
-        seen = {}
-        for i, n in enumerate(nums):
-            if target - n in seen:
-                return ([seen[target - n], i])
-            elif n not in seen:
-                seen[n] = i​
